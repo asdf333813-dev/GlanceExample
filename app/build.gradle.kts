7f0b1b8074961e7656d27569292a4444c8058cab
Андрей <asdf333813@gmail.com>
2026-06-13 20:27:39 +0700

    Add Glance dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,4 +51,6 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
     debugImplementation(libs.androidx.compose.ui.tooling)
+    implementation(libs.androidx.glance.appwidget)
+    implementation(libs.androidx.glance.material3)
 }
