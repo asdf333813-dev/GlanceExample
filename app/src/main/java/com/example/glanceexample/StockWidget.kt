31ef9ad6c700643cc36039c64d57b9c7ff29e28e
Андрей <asdf333813@gmail.com>
2026-06-13 21:13:45 +0700

    Display stock price in widget

--- app/src/main/java/com/example/glanceexample/StockWidget.kt
+++ app/src/main/java/com/example/glanceexample/StockWidget.kt
@@ -1,10 +1,24 @@
 package com.example.glanceexample
 
 import android.content.Context
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.glance.GlanceId
+import androidx.glance.GlanceModifier
+import androidx.glance.GlanceTheme
 import androidx.glance.appwidget.GlanceAppWidget
 import androidx.glance.appwidget.provideContent
+import androidx.glance.background
+import androidx.glance.layout.Column
+import androidx.glance.layout.fillMaxSize
+import androidx.glance.layout.padding
+import androidx.glance.text.FontWeight
 import androidx.glance.text.Text
+import androidx.glance.text.TextStyle
+import java.util.Locale
 
 class StockWidget : GlanceAppWidget() {
 
@@ -12,8 +26,50 @@ class StockWidget : GlanceAppWidget() {
         context: Context,
         id: GlanceId
     ) {
+        PriceDataRepo.update()
+
         provideContent {
-            Text("GOOGL")
+            GlanceTheme {
+                GlanceContent()
+            }
+        }
+    }
+
+    @Composable
+    fun GlanceContent() {
+        val price by PriceDataRepo.currentPrice.collectAsState()
+
+        Column(
+            modifier = GlanceModifier
+                .fillMaxSize()
+                .background(GlanceTheme.colors.background)
+                .padding(8.dp)
+        ) {
+            Text(
+                text = PriceDataRepo.ticker,
+                style = TextStyle(
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.Bold
+                )
+            )
+
+            Text(
+                text = String.format(Locale.getDefault(), "%.2f", price),
+                style = TextStyle(
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = GlanceTheme.colors.error
+                )
+            )
+
+            Text(
+                text = "${PriceDataRepo.change} %",
+                style = TextStyle(
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = GlanceTheme.colors.error
+                )
+            )
         }
     }
 }
